b189c631593354d206b632c6180a62914be7278d
IMac3 <IMac3>
2023-01-30 10:38:50 +0300

    Добавьте файлы проекта.

---
+++ ClothingStore/Pages/PublicPages/Input.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClothingStore.DB.Models;
+
+namespace ClothingStore.Pages.PublicPages
+{
+    /// <summary>
+    /// Логика взаимодействия для Input.xaml
+    /// </summary>
+    public partial class Input : Page
+    {
+        public Input()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (TBName.Text.Length > 0)
+            {
+                if (TBPass.Text.Length > 0)
+                {
+                    DataTable dt_
+                }    
+            }
+        }
+    }
+}
